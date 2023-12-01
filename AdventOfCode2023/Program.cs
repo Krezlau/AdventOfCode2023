c06f3eef2eeeb2703dcfa9ee19f89559e8f9b33d
Krzysztof <krzysztof.andrzej.jurkowski@gmail.com>
2023-12-01 11:06:18 +0100

    first part of day 1

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("hello");
+var input = File.ReadAllLines("Data/input.txt");
+
+input = input.Select(line => string.Concat(line.Where(character => char.IsDigit(character)))).ToArray();
+int[] values = input.Select(line => int.Parse(line[0].ToString()) * 10 + int.Parse(line[^1].ToString())).ToArray();
+
+Console.WriteLine(values.Sum());
