5d63c7767fe0994a73fe6f3851e64132bc3c6dae
Krzysztof <krzysztof.andrzej.jurkowski@gmail.com>
2023-12-01 14:47:47 +0100

    first day done

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -2,7 +2,29 @@
 
 var input = File.ReadAllLines("Data/input.txt");
 
-input = input.Select(line => string.Concat(line.Where(character => char.IsDigit(character)))).ToArray();
-int[] values = input.Select(line => int.Parse(line[0].ToString()) * 10 + int.Parse(line[^1].ToString())).ToArray();
+Console.WriteLine(
+    input
+        .Select(line => 
+            string.Concat(line.Where(character => char.IsDigit(character))))
+        .Select(line => 
+            int.Parse(line[0].ToString()) * 10 + int.Parse(line[^1].ToString()))
+        .Sum());
 
-Console.WriteLine(values.Sum());
+// part two
+//input = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen".Split("\n");
+
+Console.WriteLine(
+    input
+        .Select(line => string.Concat(GetDigitsFromLine(line)
+                .Where(character => char.IsDigit(character))))
+        .Select(line => 
+                int.Parse(line[0].ToString()) * 10 + int.Parse(line[^1].ToString()))
+        .Sum());
+
+string GetDigitsFromLine(string line)
+{
+    string[] digits = new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    for (int i = 0; i < digits.Length; i++)
+        line = line.Replace(digits[i], digits[i].Substring(0, 2) + (i + 1) + digits[i].Substring(2));
+    return line;
+}
