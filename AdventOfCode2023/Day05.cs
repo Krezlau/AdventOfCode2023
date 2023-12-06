2676b5cd40cc11c185d4ed593a13b81ea22d10ff
Krzysztof <krzysztof.andrzej.jurkowski@gmail.com>
2023-12-06 09:24:25 +0100

    day05 part two - done yesterday, forgot to commit

--- AdventOfCode2023/Day05.cs
+++ AdventOfCode2023/Day05.cs
@@ -1,4 +1,8 @@
-﻿namespace AdventOfCode2023;
+﻿using System.Collections.Concurrent;
+using System.Numerics;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace AdventOfCode2023;
 
 public class Day05 : Day
 {
@@ -7,6 +11,7 @@ public class Day05 : Day
     }
 
     private List<long> seeds = new();
+    private ConcurrentBag<long> seeds2 = new();
     private List<List<Mapping>> _filters = new();
     public override void ExecutePartOne()
     {
@@ -22,14 +27,63 @@ public class Day05 : Day
 
     public override void ExecutePartTwo()
     {
-        throw new NotImplementedException();
+        var threads = new List<Thread>();
+        var seedsData = Data[0].Split(": ")[1].Split(" ");
+        for (int j = 0; j < seedsData.Length - 1; j += 2)
+        {
+            int xd = j;
+            threads.Add(new Thread(() => part2(xd)));
+            threads[^1].Start();
+        }
+        
+        while (threads.Any(t => t.IsAlive))
+        {
+            Thread.Sleep(100);
+        }
     }
 
-    private void ParseData()
+    private void part2(int j)
     {
-        foreach (string seed in Data[0].Split(": ")[1].Split(" "))
+        List<long> seeds = new();
+        var seedsData = Data[0].Split(": ")[1].Split(" ");
+        var one = long.Parse(seedsData[j]);
+        var two = long.Parse(seedsData[j + 1]);
+        for (var x = one; x < one + two; x++)
         {
-            seeds.Add(long.Parse(seed));
+            seeds.Add(x);
+        }
+        foreach (var filters in _filters)
+        {
+            seeds = seeds.Select(s => PassThroughFilters(s, filters)).ToList();
+        }
+        Console.WriteLine(seeds.Min());
+    }
+
+    private void ParseData(bool secondPart = false)
+    {
+        if (!secondPart)
+            foreach (string seed in Data[0].Split(": ")[1].Split(" "))
+            {
+                seeds.Add(long.Parse(seed));
+            }
+        else
+        {
+            var seedsData = Data[0].Split(": ")[1].Split(" ");
+            var threads = new List<Thread>();
+            for (int j = 0; j < seedsData.Length - 1; j+=2)
+            {
+                //multithread 
+                int xd = j;
+                threads.Add(new Thread(() => AddToList(seedsData.ToArray(), xd)));
+                threads[^1].Start();
+            }
+
+            while (threads.Any(t => t.IsAlive))
+            {
+                Thread.Sleep(100);
+            }
+            
+            Console.WriteLine(seeds2.Count);
         }
         
         int i = -1;
@@ -50,7 +104,7 @@ public class Day05 : Day
     
     private long PassThroughFilters(long seed, List<Mapping> filters)
     {
-        long ret = seed;
+        var ret = seed;
         foreach (var filter in filters)
         {
             ret = filter.ConvertSeed(seed);
@@ -60,6 +114,17 @@ public class Day05 : Day
             }
         }
         return seed;
+    }
+    
+    private void AddToList(string[] seedsData, int j)
+    {
+        Console.WriteLine(j);
+        var one = long.Parse(seedsData[j]);
+        var two = long.Parse(seedsData[j + 1]);
+        for (var x = one; x < one + two; x++)
+        {
+            seeds2.Add(x);
+        }
     }
 
     private class Mapping
@@ -83,6 +148,42 @@ public class Day05 : Day
                 return Destination + (seed - Source);
             }
             return seed;
+        }
+
+        public (Range? unchanged, Range? changed, Range? unchanged2) ConvertRange(Range seed)
+        {
+            // split range if it is necessary
+            if (seed.Start >= Source && seed.End < Source + Range)
+            {
+                return (null, new Range(Destination + (seed.Start - Source), Destination + (seed.End - Source)), null);
+            }
+            if (seed.End < Source || seed.Start >= Source + Range)
+            {
+                return (seed, null, null);
+            }
+            if (seed.Start >= Source)
+            {
+                return (new Range(Source+Range, seed.End), new Range(Destination + (seed.Start - Source), Destination +  Range),null);
+            }
+            return (new Range(seed.Start, Source - 1), new Range(Source, Destination + Range - 1), new Range(Source + Range, seed.End));
+        }
+    }
+
+    private class Range
+    {
+        public long Start { get; set; }
+        public long End { get; set; }
+        
+        public Range(string valueOne, string valueTwo)
+        {
+            Start = long.Parse(valueOne);
+            End = Start + long.Parse(valueTwo);
+        }
+        
+        public Range(long valueOne, long valueTwo)
+        {
+            Start = valueOne;
+            End = valueTwo;
         }
     }
 }
